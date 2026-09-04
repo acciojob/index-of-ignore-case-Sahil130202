b761fc7c995cd1983307261b0fccdd500e5223d7
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-04 13:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let s11 = s1.toLowerCase()
+	let s22 = s2.toLowerCase()
+
+	return s11.indexOf(s22)
+	}
+
+
+
+
+
+
+	
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+/alert(indexOfIgnoreCase(s1, s2));
